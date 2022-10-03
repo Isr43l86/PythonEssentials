5900aa9546a2263def9c9d12239efb884b86ba33
Isr43l86 <anthonyalmachi@gmail.com>
2022-10-03 18:47:59 -0500

    Message

---
+++ Ejercicios/input.py
@@ -0,0 +1,7 @@
+# pedir por consola el nombre apellido, ubicacion, edad
+first_name = input("Ingrese su primer nombre -> ")
+last_name = input("Ingrese su primer apellido -> ")
+location = input("Ingrese su ubicación -> ")
+age = input("ingrese su edad -> ")
+
+print("\n\nEl usuario {0} {1} vive en {2} y tiene {3} años de edad\n\n".format(first_name, last_name, location, age))

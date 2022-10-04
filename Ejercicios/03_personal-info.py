e81d9b39f8b58f61bc0d9e2891f8e02a37ef2890
Isr43l86 <anthonyalmachi@gmail.com>
2022-10-03 20:41:03 -0500

    Clase 03/10/2022

--- Ejercicios/03_personal-info.py
+++ Ejercicios/03_personal-info.py
@@ -3,5 +3,6 @@ first_name = input("Ingrese su primer nombre -> ")
 last_name = input("Ingrese su primer apellido -> ")
 location = input("Ingrese su ubicación -> ")
 age = input("ingrese su edad -> ")
+space = " "
 
-print("\n\nEl usuario {0} {1} vive en {2} y tiene {3} años de edad\n\n".format(first_name, last_name, location, age))
+print(first_name+space+last_name+space+location+space+age)

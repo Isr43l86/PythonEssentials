5a94e4d938e447de24b66c72264a731d09e7c3c8
Isr43l86 <anthonyalmachi@gmail.com>
2022-10-04 20:35:42 -0500

    Clase 04/10/2022

---
+++ Ejercicios_en_clase/kwargfun.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 29 22:07:27 2021
+
+@author: Juan Carlos
+
+"""
+## Con dos asteriscos se puede recibir un diccionario de objetos
+def keyw(**datos):
+    print("\nTipo de datos del argumento:",
+          type(datos))
+
+    for key, value in datos.items():
+        print("{} is {}".format(key,value))
+
+keyw(Firstname="Juan", 
+     Lastname="Domínguez", 
+     Age=42, 
+     Phone=1234567890)
+keyw(Firstname="John", 
+     Lastname="Wood",
+     Email="johnwood@nomail.com",
+     Country="Wakanda", 
+     Age=25, 
+     Phone=9876543210)
